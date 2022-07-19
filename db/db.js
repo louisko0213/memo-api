922ab6e66dc7fd1b9f5020783452efd8e2946ee3
Louis <louis0975775227@gmail.com>
2022-07-19 14:39:07 +0800

    edit db

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: '',
-    database: 'memoSystem'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bcf2bf5a93ec0c',
+    password: 'ca9e28e4',
+    database: 'heroku_5e85143b3285cab'
 });
 
 module.exports = db;
