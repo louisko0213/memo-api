4f93861c2d7590f3364ed3ebec65ac74f81198f3
Louis <louis0975775227@gmail.com>
2022-07-19 15:23:55 +0800

    ok

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use(memoRoutes);
 
-app.listen(3002, () => {
-    console.log('server is running on port 3002');
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
